26fa56ec16bef511412442f7976e95e85616bb54
Alexandre Segura <mex.zktk@gmail.com>
2019-08-05 21:50:22 +0200

    Remove JwtSubscriber.

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -269,7 +269,6 @@ class AdminController extends Controller
             'date' => $date,
             'orders_normalized' => $ordersNormalized,
             'routes' => $request->attributes->get('routes'),
-            'jwt' => $request->getSession()->get('_jwt'),
             'preparation_delay' => intval($preparationDelay),
         ]);
     }
